c3731a0bc5b48cdad3c54f53ad7a82586b5eab4c
David McIntyre <davidrobert.mcintyre@gmail.com>
2023-11-28 16:47:50 +0100

    created api.js

---
+++ src/api.js
@@ -0,0 +1,10 @@
+ export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '8690182e82msh15f67f4bcce826ap19388bjsneae56d2bca46',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
