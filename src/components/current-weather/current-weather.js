0997bcf777f795952faf8438bf1a435f477fa57a
David McIntyre <davidrobert.mcintyre@gmail.com>
2024-01-21 18:55:39 +0100

    created the basic elements and classes for the component, imported react and stylesheet.

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -1,7 +1,18 @@
-import "./current-weather.css";
+import React from "react";
+import './current-weather.css';
 
 const CurrentWeather = () => {
-    return "hello";
+    return (
+        <div className='weather'>
+            <div className='top'>
+                <div>
+                <p className='city'>Belgrade</p>
+                <p className='weather-decription'>Sunny</p>
+                </div>
+                <img alt='weather' className='weather-icon' src='icons/01d.png'/>
+            </div>
+        </div>
+    );
 }
 
 export default CurrentWeather;
